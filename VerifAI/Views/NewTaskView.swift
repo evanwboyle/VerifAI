6812c211c346263cfc26ce2a25b84fdabf71a594
evanwboyle <evan.wesley.boyle@gmail.com>
2025-11-07 20:34:40 -0500

    Add initial view files for Home, Camera, NewTask, Restrict, and Settings

---
+++ VerifAI/Views/NewTaskView.swift
@@ -0,0 +1,18 @@
+//
+//  NewTaskView.swift
+//  VerifAI
+//
+//  Created by Evan Boyle on 11/7/25.
+//
+
+import SwiftUI
+
+struct NewTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewTaskView()
+}
---
+++ VerifAI/Views/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  VerifAI
+//
+//  Created by Evan Boyle on 11/7/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
